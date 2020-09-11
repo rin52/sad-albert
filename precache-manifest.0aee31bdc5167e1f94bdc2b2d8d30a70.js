b163f1ac71813fc6cc071e08370c05eed03a793d
rin52 <sheenafan1@gmail.com>
2020-09-10 23:19:08 -0600

    Updates

---
+++ precache-manifest.0aee31bdc5167e1f94bdc2b2d8d30a70.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a53493d58b8447c1e14ec5b4faf52e",
+    "url": "/sad-albert/index.html"
+  },
+  {
+    "revision": "b409b6309c8ce66362a0",
+    "url": "/sad-albert/static/css/main.52054afd.chunk.css"
+  },
+  {
+    "revision": "48d1d5cb2956f62c7092",
+    "url": "/sad-albert/static/js/2.e27a1791.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sad-albert/static/js/2.e27a1791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b409b6309c8ce66362a0",
+    "url": "/sad-albert/static/js/main.fa130aea.chunk.js"
+  },
+  {
+    "revision": "9a4bcd9e1724a49568ca",
+    "url": "/sad-albert/static/js/runtime-main.df777153.js"
+  },
+  {
+    "revision": "f2fcad647ef8f190c26f99da8bc96437",
+    "url": "/sad-albert/static/media/Aether.f2fcad64.svg"
+  },
+  {
+    "revision": "d3ddff78ccd4fa5e4e46a666f0e31459",
+    "url": "/sad-albert/static/media/Caelum.d3ddff78.svg"
+  },
+  {
+    "revision": "949b3d046cdc27deded5684cf465006a",
+    "url": "/sad-albert/static/media/Fulgur.949b3d04.svg"
+  },
+  {
+    "revision": "340949738ca1981c056c4aa02773dffd",
+    "url": "/sad-albert/static/media/Hydragenum.34094973.svg"
+  },
+  {
+    "revision": "e3bf3d1b499f2fae0969761f68eb9c02",
+    "url": "/sad-albert/static/media/Quebrith.e3bf3d1b.svg"
+  },
+  {
+    "revision": "30f7ba0cbcc0bce108b31aad6f603827",
+    "url": "/sad-albert/static/media/Rebis.30f7ba0c.svg"
+  },
+  {
+    "revision": "5425061c24fb1b6c1d90082c364cc61a",
+    "url": "/sad-albert/static/media/Sol.5425061c.svg"
+  },
+  {
+    "revision": "382e7b325a3ebe59cba10e9222dcf0a3",
+    "url": "/sad-albert/static/media/Vermilion.382e7b32.svg"
+  },
+  {
+    "revision": "c8ee1bdb7cd126313da03f2c8454fd79",
+    "url": "/sad-albert/static/media/Vitriol.c8ee1bdb.svg"
+  },
+  {
+    "revision": "d08551f26037d01c5904c2e15ebdead4",
+    "url": "/sad-albert/static/media/blueMutagen.d08551f2.png"
+  },
+  {
+    "revision": "2f05120696907fde0ef2e27c88d8f769",
+    "url": "/sad-albert/static/media/greenMutagen.2f051206.png"
+  },
+  {
+    "revision": "dadefeffd845de0000e3a60646221091",
+    "url": "/sad-albert/static/media/logo.dadefeff.svg"
+  },
+  {
+    "revision": "194226831dfeb52bce935af9b085e499",
+    "url": "/sad-albert/static/media/redMutagen.19422683.png"
+  },
+  {
+    "revision": "1bb9c8480970e137366449e33ed58068",
+    "url": "/sad-albert/static/media/titleLogo.1bb9c848.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8141a636f254eca95cec8c4ad1433a87.js"
+  "/sad-albert/precache-manifest.0aee31bdc5167e1f94bdc2b2d8d30a70.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sad-albert/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
