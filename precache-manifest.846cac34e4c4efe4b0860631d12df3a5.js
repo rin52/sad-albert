21c5cbb2f4570bb703bb1a812aed480d18f47681
rin52 <sheenafan1@gmail.com>
2020-09-10 22:48:03 -0600

    Updates

---
+++ precache-manifest.846cac34e4c4efe4b0860631d12df3a5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b58226a33470f197e194d634a82d84",
+    "url": "./index.html"
+  },
+  {
+    "revision": "06aec549d24b529d94c8",
+    "url": "./static/css/main.52054afd.chunk.css"
+  },
+  {
+    "revision": "48d1d5cb2956f62c7092",
+    "url": "./static/js/2.e27a1791.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.e27a1791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06aec549d24b529d94c8",
+    "url": "./static/js/main.5cf75077.chunk.js"
+  },
+  {
+    "revision": "b029b255ca8905a93f40",
+    "url": "./static/js/runtime-main.28742bd1.js"
+  },
+  {
+    "revision": "f2fcad647ef8f190c26f99da8bc96437",
+    "url": "./static/media/Aether.f2fcad64.svg"
+  },
+  {
+    "revision": "d3ddff78ccd4fa5e4e46a666f0e31459",
+    "url": "./static/media/Caelum.d3ddff78.svg"
+  },
+  {
+    "revision": "949b3d046cdc27deded5684cf465006a",
+    "url": "./static/media/Fulgur.949b3d04.svg"
+  },
+  {
+    "revision": "340949738ca1981c056c4aa02773dffd",
+    "url": "./static/media/Hydragenum.34094973.svg"
+  },
+  {
+    "revision": "e3bf3d1b499f2fae0969761f68eb9c02",
+    "url": "./static/media/Quebrith.e3bf3d1b.svg"
+  },
+  {
+    "revision": "30f7ba0cbcc0bce108b31aad6f603827",
+    "url": "./static/media/Rebis.30f7ba0c.svg"
+  },
+  {
+    "revision": "5425061c24fb1b6c1d90082c364cc61a",
+    "url": "./static/media/Sol.5425061c.svg"
+  },
+  {
+    "revision": "382e7b325a3ebe59cba10e9222dcf0a3",
+    "url": "./static/media/Vermilion.382e7b32.svg"
+  },
+  {
+    "revision": "c8ee1bdb7cd126313da03f2c8454fd79",
+    "url": "./static/media/Vitriol.c8ee1bdb.svg"
+  },
+  {
+    "revision": "d08551f26037d01c5904c2e15ebdead4",
+    "url": "./static/media/blueMutagen.d08551f2.png"
+  },
+  {
+    "revision": "2f05120696907fde0ef2e27c88d8f769",
+    "url": "./static/media/greenMutagen.2f051206.png"
+  },
+  {
+    "revision": "dadefeffd845de0000e3a60646221091",
+    "url": "./static/media/logo.dadefeff.svg"
+  },
+  {
+    "revision": "194226831dfeb52bce935af9b085e499",
+    "url": "./static/media/redMutagen.19422683.png"
+  },
+  {
+    "revision": "1bb9c8480970e137366449e33ed58068",
+    "url": "./static/media/titleLogo.1bb9c848.svg"
+  }
+]);
