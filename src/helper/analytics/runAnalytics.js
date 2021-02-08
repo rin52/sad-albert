189bb53f56a835b674d176ed9ae1b0c5f0b39975
rin52 <sheenafan1@gmail.com>
2021-02-07 22:10:42 -0600

    remove analytic config.

--- src/helper/analytics/runAnalytics.js
+++ src/helper/analytics/runAnalytics.js
@@ -1,6 +1,5 @@
 export default function runAnalytics(page) {
     if (process.env.REACT_APP_SIM === undefined) {
-        window.gtag('config', 'G-QY4K1FJZTN', { 'page_title': document.title, page_path: '/sad-albert/' });
         window.gtag('event', 'screen_view', {
             'app_name': 'Sad-Albert',
             'screen_name': page
